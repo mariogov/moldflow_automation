eee26e650b2839676c2e71112c9fdbead5703495
oskay-sozen <oskay.sozen@gmail.com>
2025-08-18 01:11:23 +0200

    Data loader updated

--- utils.py
+++ utils.py
@@ -77,3 +77,14 @@ def extract_switch_time_from_log(log_path):
                         break
                 i -= 1
     raise ValueError(f"Switch time not found in log: {log_path}")
+
+def calculate_area_between_curves(real_time, real_disp, guess_time, guess_disp):
+    common_time = np.linspace(0.0, max(real_time.max(), guess_time.max()), num=200)
+
+    # Resample both curves on this time grid
+    real_disp_resampled = np.interp(common_time, real_time, real_disp)
+    guess_disp_resampled = np.interp(common_time, guess_time, guess_disp)
+
+    mse = np.mean((real_disp_resampled-guess_disp_resampled)**2)
+    
+    return mse
